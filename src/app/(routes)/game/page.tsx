2611f72fa7b78b546c4c3dc930224cc4cf59044b
Big ok <anthonyyunli@gmail.com>
2025-06-25 20:09:46 -0600

    frontend bugs

--- src/app/(routes)/game/page.tsx
+++ src/app/(routes)/game/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { state } from "@/game/state"
+import { state } from "@/game/state";
 import { useSnapshot } from "valtio";
 import { useState } from "react";
 
-import ActionButton from "@/app/components/ui/ActionButton"
-import AgeUp from "@/app/components/ui/AgeUp"
-import ProfileIcon from "@/app/components/ui/ProfileIcon"
+import ActionButton from "@/app/components/ui/ActionButton";
+import AgeUp from "@/app/components/ui/AgeUp";
+import ProfileIcon from "@/app/components/ui/ProfileIcon";
 import PopUp from "@/app/components/events/Event";
 import Transcripts from "@/app/components/ui/Transcripts";
 
@@ -16,7 +16,7 @@ export default function Game() {
 
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        console.log(snap)
+        console.log(snap);
         state.name = inputName;
         console.log("Player name set to:", snap.name);
     }
@@ -25,25 +25,42 @@ export default function Game() {
         <>
             <div className="bg-[radial-gradient(circle,rgba(105,105,105)_0%,black_50%)] min-h-screen">
                 <div className="p-5">
-                    <ProfileIcon name={snap.name} job="Software Engineer" stress={snap.stress} money={snap.money} />
+                    <ProfileIcon
+                        name={snap.name}
+                        job="Software Engineer"
+                        stress={snap.stress}
+                        money={snap.money}
+                    />
                 </div>
 
                 <div className="flex">
                     <ul>
-                        <li><ActionButton text="Education" /></li>
-                        <li><ActionButton text="Job" /></li>
-                        <li><ActionButton text="Assets" /></li>
-                        <li className="p-4"><AgeUp /></li>
+                        <li>
+                            <ActionButton text="Education" />
+                        </li>
+                        <li>
+                            <ActionButton text="Job" />
+                        </li>
+                        <li>
+                            <ActionButton text="Assets" />
+                        </li>
+                        <li className="p-4">
+                            <AgeUp />
+                        </li>
                     </ul>
 
                     {/* Right content */}
                     <div className="flex flex-1 justify-end items-start p-8">
-                        <Transcripts messages={snap.events} />
+                        <Transcripts messages={state.events} />
                     </div>
                 </div>
             </div>
 
-            <PopUp name="Test Event" description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum excepturi dolore voluptates dolores magnam nemo possimus vero illum exercitationem nobis! Facilis quod provident sunt esse sapiente eum quos itaque ratione." icon="🚀" />
+            <PopUp
+                name="Test Event"
+                description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum excepturi dolore voluptates dolores magnam nemo possimus vero illum exercitationem nobis! Facilis quod provident sunt esse sapiente eum quos itaque ratione."
+                icon="🚀"
+            />
 
             {/* 
             <div className="h-full w-full bg-black/50 p-4">
@@ -55,5 +72,5 @@ export default function Game() {
             </div>
             */}
         </>
-    )
+    );
 }
