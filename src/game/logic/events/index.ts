8eadaae389a362f34eb5e11a646b78d16023fd56
Alexus-404 <164604694+Alexus-404@users.noreply.github.com>
2025-06-26 12:42:52 -0600

    Added new types

--- src/game/logic/events/index.ts
+++ src/game/logic/events/index.ts
@@ -1,33 +1,51 @@
 import { gameEvents } from "./data";
-import { pickWeighted } from "@/utils";
+import { GameEvent, ScheduledEvent, RandomEvent } from "./eventsClasses";
+import { pickWeighted, Queue } from "@/utils";
 
 let triggeredEvents = new Set<string>();
 
+function isScheduledEvent(event: GameEvent): event is ScheduledEvent {
+  return event instanceof ScheduledEvent;
+}
+
+function isRandomEvent(event: GameEvent): event is RandomEvent {
+  return event instanceof RandomEvent;
+}
+
 export function handleEvents(currentAge: number) {
+  let eventsQueue = new Queue<GameEvent>();
+
   // Scheduled Events
   for (const event of gameEvents) {
     if (
-      event.type === "scheduled" &&
+      isScheduledEvent(event) &&
       event.triggerAge === currentAge &&
       !triggeredEvents.has(event.name)
     ) {
-      if (event.execute) event.execute();
+      eventsQueue.enqueue(event);
       triggeredEvents.add(event.name);
     }
   }
 
-  // Random Events
-  const eligibleRandomEvents = gameEvents.filter(
-    (event) =>
-      event.type === "random" &&
-      (event.condition ? event.condition() : true) &&
-      !triggeredEvents.has(event.name) && // Optional: prevent repeats
-      (event.weight ?? 0) > 0
-  );
+  if (eventsQueue.empty()) {
+    // Random Events
+    const eligibleRandomEvents = gameEvents.filter(
+      (event): event is RandomEvent =>
+        isRandomEvent(event) &&
+        (event.condition ? event.condition() : true) &&
+        !triggeredEvents.has(event.name) && // Optional: prevent repeats
+        (typeof event.weight === "function" ? event.weight() : event.weight) > 0
+    );
 
-  const picked = pickWeighted(eligibleRandomEvents);
-  if (picked) {
-    if (picked.execute) picked.execute();
-    triggeredEvents.add(picked.name);
+    const picked = pickWeighted(eligibleRandomEvents);
+    if (picked) {
+      picked.execute();
+      triggeredEvents.add(picked.name);
+    }
+  }
+
+  // Execute functions
+  while(!eventsQueue.empty()) {
+    eventsQueue.dequeue()?.execute();
   }
 }
