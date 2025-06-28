f52b4e52e201f8627df573409d054c36f21351bf
Big ok <anthonyyunli@gmail.com>
2025-06-27 23:21:30 -0600

    minor

--- src/app/(routes)/game/page.tsx
+++ src/app/(routes)/game/page.tsx
@@ -3,39 +3,51 @@
 import { state } from "@/game/state";
 import { useSnapshot } from "valtio";
 
-import { AgeUp, ProfileIcon, Transcripts, EducationInfo, JobInfo, FinanceInfo, ProductsDisplay, GameOverDialog, EventManager } from "@/components/exports";
+import {
+    AgeUp,
+    ProfileIcon,
+    Transcripts,
+    EducationInfo,
+    JobInfo,
+    FinanceInfo,
+    ProductsDisplay,
+    GameOverDialog,
+    EventManager,
+} from "@/components/exports";
+import { Notifier } from "@/components/Notifier";
 
 export default function Game() {
     const snap = useSnapshot(state);
 
     return (
         <>
+            <Notifier />
             <GameOverDialog />
-            {snap.alive && snap.event && (<EventManager event={snap.event} />)}
-            {/* <Button className="fixed bottom-4 right-4" variant="outline" onClick={() => { console.log(snap.event) }}>Test</Button> */}
+            {snap.alive && snap.event && <EventManager event={snap.event} />}
 
-            <div className="bg-[radial-gradient(circle,rgba(105,105,105)_0%,black_50%)] min-h-screen">
-                <div className="min-h-screen">
+            <div className="bg-[radial-gradient(circle,rgba(105,105,105)_0%,black_50%)] min-h-screen flex">
+                <div className="w-2/3 min-h-screen flex flex-col justify-between">
                     {/* Header */}
-                    <div className="p-5 flex items-center gap-8">
-                        <ProfileIcon />
-                        <div className="flex-1">
-                            <ProductsDisplay />
-                        </div>
-                    </div>
-
-                    <div className="flex">
-                        <div className="flex flex-1 justify-end items-start p-8">
-                            <Transcripts messages={state.transcript} />
-                        </div>
-                    </div>
-
-                    <div className="fixed bottom-10 left-5 flex gap-4">
-                        <AgeUp />
+                    <div className="p-5 flex items-center justify-between gap-8">
                         <EducationInfo education={snap.education} />
                         {snap.job && <JobInfo job={snap.job} />}
                         <FinanceInfo />
+                        <AgeUp />
                     </div>
+
+                    {/* Main content */}
+                    <div className="flex-1 p-5">
+                        <ProductsDisplay />
+                    </div>
+
+                    {/* Fixed bottom profile icon */}
+                    <div className="fixed bottom-10 left-5 flex gap-4">
+                        <ProfileIcon />
+                    </div>
+                </div>
+
+                <div className="w-1/3 min-h-screen p-8">
+                    <Transcripts messages={state.transcript} />
                 </div>
             </div>
         </>
