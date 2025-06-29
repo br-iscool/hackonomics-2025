885770cbb818819e4ee21e6cd17bec94ff200ce5
Big ok <anthonyyunli@gmail.com>
2025-06-29 12:54:31 -0600

    colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"]
+  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        success: "oklch(67.047% 0.22276 143.238)",
+        warning: "oklch(83.468% 0.17124 82.477)",
+        error: "oklch(57.7% 0.245 27.325)",
+        info: "oklch(75.402% 0.00009 271.152)",
+      },
+    },
+  },
 };
+
 export default config;
